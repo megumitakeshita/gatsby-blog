af55938cc048709927d3e0c515031b9eab86dd0d
cbltakeshitamegumitakeshita <megumi_takeshita@canbethelight.com>
2022-04-05 14:31:16 +0900

    4/5更新分

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+const path = require('path');
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
 
 module.exports = {
   siteMetadata: {
@@ -6,5 +11,27 @@ module.exports = {
     author: "Engineer X"
   },
   plugins: [
-  ]
+    {
+      resolve: `gatsby-source-microcms`,
+      options: {
+        apiKey: process.env.API_KEY,
+        serviceId: 'cbltest',
+        apis: [
+          {
+            endpoint: "blog",
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        commonmark: true,
+        footnotes: true,
+        pedantic: true,
+        gfm: true,
+        plugins: [],
+      },
+    },
+  ],
 }
