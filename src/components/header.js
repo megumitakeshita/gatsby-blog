ba192c04cb7aa956dc089c74b3182f274cf68856
cbltakeshitamegumitakeshita <megumi_takeshita@canbethelight.com>
2022-04-06 12:18:06 +0900

    4/6-6

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,12 @@
 import React from "react"
 import {Link} from "gatsby"
-import LOGO_IMG from "../../static/logo.png"
+// import LOGO_IMG from "../../static/logo.png"
 import "../styles/header.css"
 
 export default function Header(){
     return(
         <header>
-            <Link to="/"> ロゴ入る </Link>
+            <Link to="/">ロゴ挿入場所</Link>
         </header>
     )
 }
